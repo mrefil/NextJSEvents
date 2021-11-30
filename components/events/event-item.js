81a245c8b8067373c909c3c8f97595f8e72801ae
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-11-30 18:13:43 -0500

    add styling

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,33 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
-    const {title, image, date, location, id} = props;
+  const { title, image, date, location, id } = props;
 
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    })
-    const formattedAddress = location.replace(', ', '\n');
-    const exploreLink = `/events/${id}`;
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
 
-    return(
-        <li>
-            <img src={'/' + image} alt="" />
-            <div>
-                <h2>{title}</h2>
-                <div>
-                    <time>{humanReadableDate}</time>
-                </div>
-                <div>
-                    <address>{formattedAddress}</address>
-                </div>
-                <div>
-                    <Link href={exploreLink}>Explore Event</Link>
-                </div>
-            </div>
-        </li>
-    )
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt="" />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
+          <div className={classes.actions}>
+            <Link href={exploreLink}>Explore Event</Link>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem;
