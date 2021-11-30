ccd09c44cc5d9865ff5fd80fd0af2f952a24b524
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-11-30 18:00:58 -0500

    add props and print data on homepage

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,13 +1,22 @@
-import EvenItem from "./event-item";
+import EventItem from './event-item';
 
 function EventList(props) {
-    const {items} = props;
-
-    return(
-        <ul>
-            {items.map(event => <EvenItem />)}
-        </ul>
-    )
-}
+    const { items } = props;
+  
+    return (
+      <ul>
+        {items.map((event) => (
+          <EventItem
+            key={event.id}
+            id={event.id}
+            title={event.title}
+            location={event.location}
+            date={event.date}
+            image={event.image}
+          />
+        ))}
+      </ul>
+    );
+  }
 
 export default EventList;
