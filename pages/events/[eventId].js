b7f25002aa3355046b3656e3920343b74f7eba37
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-11-30 20:59:52 -0500

    event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,33 @@
-function EventsDetailPage() {
-    <div>
-        <h1>Event detail Page</h1>
-    </div>
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        image={event.image}
+        address={event.location}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
-export default EventsDetailPage;
+export default EventDetailPage;
