8ab11c9d24722db3f1cb3db0f65cf506daac40c2
Mustafa E <61510151+mrefil@users.noreply.github.com>
2021-12-15 01:39:28 -0500

    SSG & Firebase connection

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
-import {getFeaturedEvents} from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
 function HomePage(props) {
-    // get data
-    const featuredEvents = getFeaturedEvents();
-    
     return(
         <div>
-            <EventList items={featuredEvents} />
+            <EventList items={props.events} />
         </div>
     )
+}
+
+export async function getStaticProps() {
+    const featuredEvents = await getFeaturedEvents();
+    return {
+        props: {
+            events: featuredEvents
+        }
+    }
 }
 
 export default HomePage;
