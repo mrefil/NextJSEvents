24fa32619946a2f0181abd82aec782ca5ed486b8
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-03 23:50:06 -0500

    More Error Handling

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,9 +1,14 @@
-import { MongoClient } from 'mongodb';
-import { getAllDocuments } from '../../../helpers/db-util';
+import { connectDatabase, insertDocument, getAllDocuments } from '../../../helpers/db-util';
 
 async function handler(req, res) {
     const eventId = req.query.eventId;  
-    const client = await MongoClient.connect('mongodb+srv://testUser:testUser123456@cluster0.agazq.mongodb.net/events?retryWrites=true&w=majority')
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({message: 'connectiong to the db failed!'});
+      return;
+    }
 
     if (req.method === 'POST') {
       const { email, name, text } = req.body;
@@ -16,6 +21,7 @@ async function handler(req, res) {
         text.trim() === ''
       ) {
         res.status(422).json({ message: 'Invalid input.' });
+        client.close();
         return;
       }
   
@@ -26,19 +32,23 @@ async function handler(req, res) {
         eventId
       };
 
-      const db = client.db();
-      const result = await db.collection('comments').insertOne(newComment);
-      console.log(result);
-      newComment.id = result.insertedId;
-  
-      res.status(201).json({ message: 'Added comment.', comment: newComment });
+      let result;
+      try {
+        result = insertDocument(client, 'comments', newComment);
+        newComment._id = result.insertedId;
+        res.status(201).json({ message: 'Added comment.', comment: newComment });
+      } catch (error) {
+        res.status(500).json({message: 'Inserting comment failed!'});
+      }
     }
   
     if (req.method === 'GET') {
-      const db = client.db();
-      const documents = await getAllDocuments(client, 'comments', { _id: -1 }, { eventId: eventId });
-  
-      res.status(200).json({ comments: documents });
+      try {
+        const documents = await getAllDocuments(client, 'comments', { _id: -1 }, { eventId: eventId });
+        res.status(200).json({ comments: documents });
+      } catch (error) {
+        res.status(500).json({message: 'Getting comment failed!'});
+      }
     }
     client.close();
   }
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,14 +1,5 @@
-import { MongoClient } from 'mongodb';
+import {connectDatabase, insertDocument} from '../../helpers/db-util';
 
-async function connectDatabase() {
-    const client = await MongoClient.connect('mongodb+srv://testUser:testUser123456@cluster0.agazq.mongodb.net/events?retryWrites=true&w=majority')
-    return client;
-}
-
-async function insertDocument(client, document) {
-    const db = client.db();
-    await db.collection('newsletter').insertOne(document);
-}
 
 async function handler(req, res) {
     if (req.method === 'POST') {
@@ -28,7 +19,7 @@ async function handler(req, res) {
         }
 
         try {
-            await insertDocument(client, {email: userEmail});
+            await insertDocument(client, 'newsletter', {email: userEmail});
             client.close();
         } catch (error) {
             res.status(500).json({message: 'Inserting data to db failed!'});
