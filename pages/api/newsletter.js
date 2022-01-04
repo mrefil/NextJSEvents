440115323a6b4b2a35abe648d7f36e2fa1e7642b
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-03 22:45:18 -0500

    Running MongoDB Queries From Inside API Routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,13 +1,20 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const userEmail = req.body.email;
 
-        if (!userEmail || !userEmail.includes('@')) {
-            res.status(422).json({message: 'Invalied email.'});
-            return;
-        }
+        // if (!userEmail || !userEmail.includes('@')) {
+        //     res.status(422).json({message: 'Invalied email.'});
+        //     return;
+        // }
 
-        console.log(userEmail);
+        const client = await MongoClient.connect('mongodb+srv://testUser:testUser123456@cluster0.agazq.mongodb.net/newsletter?retryWrites=true&w=majority')
+        
+        const db = client.db();
+        await db.collection('emails').insertOne({email: userEmail});
+        client.close();
+
         res.status(201).json({message: 'Signed up!'});
     }
 }
